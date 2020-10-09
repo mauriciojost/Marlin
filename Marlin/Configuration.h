05d038a2535cbc0b45f1a64cf3eec3fc1931808f
Mauricio Jost <mauriciojost@gmail.com>
2020-10-09 18:22:57 +0200

    Not working before, invert nozzle-to-probe coords

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -984,7 +984,7 @@
  *     O-- FRONT --+
  */
 // MJOST: SCENARIO 4
-#define NOZZLE_TO_PROBE_OFFSET { -15, -41, -2 }
+#define NOZZLE_TO_PROBE_OFFSET { 15, 41, -2 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
